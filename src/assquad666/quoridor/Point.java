abf82cd95fbb3592a707d76a5c4ccb22ea61173e
Sacha Beraud <sacha.beraud@gmail.com>
2011-04-27 15:02:40 +1000

    some text in Point, just a test

--- src/assquad666/quoridor/Point.java
+++ src/assquad666/quoridor/Point.java
@@ -1,5 +1,53 @@
 package assquad666.quoridor;
 
+
 public class Point {
 
+	
+	public int x, y;
+
+	public Point()
+	{
+		x = 0;
+		y = 0;
+	}
+
+	public Point(int x, int y)
+	{
+		this.x = x;
+		this.y = y;
+	}
+
+	public Point(Point point)
+	{
+		x = point.x;
+		y = point.y;
+	}
+
+	public Point subtract(Point vec)
+	{
+		return new Point(x-vec.x, y-vec.y);
+	}
+
+	public Point add(int dx, int dy)
+	{
+		return new Point(x+dx, y+dy);
+	}
+	
+	public void set(Point p)
+	{
+		x = p.x;
+		y = p.y;
+	}
+
+	@Override
+	public String toString()
+	{
+		return "{x="+x+", y="+y+"}";
+	}
+
+	public Point add(Point d)
+	{
+		return new Point(x+d.x, y+d.y);
+	}
 }

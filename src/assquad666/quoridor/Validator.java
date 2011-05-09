166f03c1039ce615db8e5df969c47cbeea74b3ec
rohan <robo.spencer@gmail.com>
2011-05-09 21:05:22 +1000

    updated Validator code, without the method stubs in other classes

--- src/assquad666/quoridor/Validator.java
+++ src/assquad666/quoridor/Validator.java
@@ -1,4 +1,4 @@
-package assquad666.quoridor;
+package quoridor;
 
 public class Validator {
 
@@ -8,7 +8,7 @@ public class Validator {
 	// you may add extra fields and methods to this class
 	// but the ProvidedTests code will only call the specified methods
 	
-	public Validator() {
+	public Validator () {
 		// TODO
 	}
 
@@ -18,12 +18,57 @@ public class Validator {
 	 * move in the list is valid,
 	 * starting from the initial position of the game.
 	 * When the game has been won, no further moves are valid.
-	 * @param moves a list of successive moves
+	 * // TODO@param moves a list of successive moves
 	 * @return validity of the list of moves
 	 */
-	public boolean check(String moves) {
-		// TODO
-		return false;
+	public boolean check (String moves) {
+		assert moves != null;
+		boolean valid = true;
+		char c;
+		char lastc = ' ';
+		
+		Game temp = new Game();
+		
+		int width = temp.width();
+		int height = temp.height();
+		
+		for (int i = 0; i < moves.length() && valid == true; i++) {
+			c = moves.charAt (i);
+			if (c != ' ') {
+				if (Character.isLetter(c) && (c - 'a' < width)) {
+					if (lastc != ' ') {
+						valid = false;
+					}
+				} else if (Character.isDigit(c - 1) && c < height) {
+					if (!Character.isLetter(lastc)) {
+						valid = false;
+					} else {
+						if ((i < (moves.length() - 1)) && (moves.charAt(i+1) == 'h' || moves.charAt(i+1) == 'v')) {
+							if (i % 2 == 0) {
+								temp.placeWall(c,lastc - 'a', moves.charAt(i+1), temp.playerOne());
+							} else {
+								temp.placeWall(c,lastc - 'a', moves.charAt(i+1), temp.playerTwo());
+							}
+							i++;
+						} else {
+							if (i % 2 == 0) {
+								temp.move(c,(lastc - 'a'), temp.playerOne());
+							} else {
+								temp.move(c,(lastc - 'a'), temp.playerTwo());
+							}
+						}
+						valid = temp.valid();
+						
+					}
+				} else {
+					valid = false;
+				}
+			}
+			lastc = c;
+		}
+	
+		
+		return valid ;
 	}
 
 }
